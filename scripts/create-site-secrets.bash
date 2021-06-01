71bc6cee91a0dbb4a7896c0385d89dab043bb668
Casey Marshall <git@cmars.tech>
2021-06-01 00:28:12 +0000

    Get htpasswd from Docker.
    htpasswd often requires installing Apache httpd. Let's avoid system deps
    like that...

--- scripts/create-site-secrets.bash
+++ scripts/create-site-secrets.bash
@@ -1,11 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -euo pipefail
 cd $(dirname $0)/..
+
+HTPASSWD="docker run --entrypoint htpasswd registry:2.7.0"
 
 umask 077
 mkdir -p secrets
 [ -f secrets/basic-auth ] || (
 	PASSWORD=$(head -c 16 /dev/urandom | base64 -w0)
 	echo $PASSWORD > secrets/ipfs-password
-	htpasswd -bc secrets/basic-auth ipfs "$PASSWORD"
+	$HTPASSWD -Bbn ipfs "$PASSWORD" > secrets/basic-auth
 )
